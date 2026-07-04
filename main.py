01b071d209f7a96bbf03138f0d77610902b808bb
yuyuPepe <yuribalestrin77@gmail.com>
2026-07-04 13:45:20 -0300

    alterações pra caixa de diálogo

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from services.sheets_service import ler_planilha
 from services.analise_service import calcular_renovacoes
 from services.grafico_service import gerar_grafico
 from services.pdf_service import gerar_pdf
+from ui.input_service import obter_aba
 
+aba = obter_aba()
 
 df = ler_planilha(
     "REMATRICULAS 2025 - 2026",
-    "07.26"
+    aba
 )
 
 resultado = calcular_renovacoes(df)
