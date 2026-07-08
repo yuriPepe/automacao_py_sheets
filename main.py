3a3778161d4f676ab6a3c20804c5ab805087b892
yuyuPepe <yuribalestrin77@gmail.com>
2026-07-08 14:44:03 -0300

    altera arquitetura feature projeto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from services.sheets_service import ler_planilha
-from automacao_py_sheets.calcular_renovacoes_por_ano.analise_service import calcular_renovacoes_por_ano, calcular_renovacoes_geral
-from automacao_py_sheets.calcular_renovacoes_por_ano.grafico_service import gerar_grafico_renovacoes_por_ano
-from automacao_py_sheets.calcular_renovacoes_por_ano.pdf_service import gerar_pdf
+from calcular_renovacoes_por_ano.analise_service import calcular_renovacoes_por_ano, calcular_renovacoes_geral
+from calcular_renovacoes_por_ano.grafico_service import gerar_grafico_renovacoes_por_ano
+from calcular_renovacoes_por_ano.pdf_service import gerar_pdf
 from ui.input_service import obter_aba, escolher_local_pdf
 
 aba = obter_aba()
