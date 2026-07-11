d81842f92015f3d991c725e7ff7e14fd9b0998af
yuyuPepe <yuribalestrin77@gmail.com>
2026-07-11 00:07:34 -0300

    botões para relaórios isolados

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from calcular_renovacoes_por_ano.analise_service import calcular_renovacoes_por_
 from calcular_renovacoes_por_ano.grafico_service import gerar_grafico_renovacoes_por_ano
 from calcular_renovacoes_por_ano.pdf_service import gerar_pdf
 from ui.input_service import obter_aba, escolher_local_pdf, escolher_relatorio
+from analise_motivos.analise_motivo_service import analise_motivos
+from analise_motivos.grafico_motivos import gerar_grafico_motivos
 
 def gerar_relatorio_renovacoes():
 
@@ -31,6 +33,22 @@ def gerar_relatorio_renovacoes():
 
     gerar_pdf(resultado_por_ano, resultado_geral, caminho_pdf, caminho_grafico)
 
+def gerar_relatorio_motivos():
+
+    aba = obter_aba()
+
+    if not aba:
+        print("operação cancelada")
+        exit()
+
+    df = ler_planilha(
+    "REMATRICULAS 2025 - 2026",
+    aba
+    )  
+
+    resultado = analise_motivos(df)
+
+    grafico_motivos = gerar_grafico_motivos(resultado)
 
 tipo = escolher_relatorio()
 
