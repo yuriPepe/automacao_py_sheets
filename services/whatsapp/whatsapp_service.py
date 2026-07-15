f7d5a8b22e2409aaad49c348099f4fc1639bd780
Yuri <yuribalestrin77@gmail.com>
2026-07-14 21:58:46 -0300

    altera config envio msgs api

--- services/whatsapp/whatsapp_service.py
+++ services/whatsapp/whatsapp_service.py
@@ -1,12 +1,13 @@
 import os
 import requests
+from whatsapp.config import WHATSAPP_TOKEN, PHONE_NUMBER_ID
 
 ACCESS_TOKEN = os.getenv("WHATSAPP_TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
 def enviar_mensagem(numero, mensagem):
 
-    url = f"https://graph.facebook.com/v23.0/{PHONE_NUMBER_ID}/messages"
+    url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
 
     headers = {
         "Authorization": f"Bearer {ACCESS_TOKEN}",
@@ -29,3 +30,7 @@ def enviar_mensagem(numero, mensagem):
     )
 
     return resposta
+
+resposta = enviar_mensagem("48988651056", "mensagem teste")
+print(resposta.status_code)
+print(resposta.json)    
