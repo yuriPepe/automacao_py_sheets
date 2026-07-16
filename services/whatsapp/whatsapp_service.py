97957396cee1d64085f0be9b6d39ef470f177b81
Yuri <yuribalestrin77@gmail.com>
2026-07-15 21:57:06 -0300

    altera whatsapp

--- services/whatsapp/whatsapp_service.py
+++ services/whatsapp/whatsapp_service.py
@@ -1,16 +1,12 @@
-import os
 import requests
-from whatsapp.config import WHATSAPP_TOKEN, PHONE_NUMBER_ID
-
-ACCESS_TOKEN = os.getenv("WHATSAPP_TOKEN")
-PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+from services.whatsapp.config import WHATSAPP_TOKEN, PHONE_NUMBER_ID
 
 def enviar_mensagem(numero, mensagem):
 
     url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
 
     headers = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Authorization": f"Bearer {WHATSAPP_TOKEN}",
         "Content-Type": "application/json"
     }
 
@@ -31,6 +27,6 @@ def enviar_mensagem(numero, mensagem):
 
     return resposta
 
-resposta = enviar_mensagem("48988651056", "mensagem teste")
+resposta = enviar_mensagem("5548988651056", "Mensagem teste")
 print(resposta.status_code)
-print(resposta.json)    
+print(resposta.json())
